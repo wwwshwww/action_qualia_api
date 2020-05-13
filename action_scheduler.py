f773c2540028882e6400782176d6233f2d88b075
Reona Sato <www.shinderu.www@gmail.com>
2020-05-14 00:47:34 +0900

    create abstract scheduler for grounded action

--- action_scheduler.py
+++ action_scheduler.py
@@ -2,12 +2,34 @@ import ros_mobile_client
 from grounded_action import GroundedAction, GroundedActionPool, GroundedStep
 
 class MapExploration(GroundedAction):
-    pass
+    def _gen_candidates(self):
+        pass
+
+    def _evaluate_poses(self, candidates):
+        pass
     
+class ElectErea(GroundedAction):
+    def _gen_candidates(self):
+        pass
+
+    def _evaluate_poses(self, candidates):
+        pass
+
+class Observation(GroundedAction):
+    def _gen_candidates(self):
+        pass
+
+    def _evaluate_poses(self, candidates):
+        pass
 
 class GAScheduler(GroundedAction):
     def __init__(self, name, client, pool):
         super().__init__(name, client)
         self.pool = pool
 
+    def _gen_candidates(self):
+        pass
+
+    def _evaluate_poses(self, candidates):
+        pass
 
