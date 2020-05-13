2e9d6285b0dc7ae42db944ff662472989b104e23
Reona Sato <www.shinderu.www@gmail.com>
2020-05-14 00:57:49 +0900

    add sample main

--- action_scheduler.py
+++ action_scheduler.py
@@ -1,3 +1,4 @@
+import roslibpy
 import ros_mobile_client
 from grounded_action import GroundedAction, GroundedActionPool, GroundedStep
 
@@ -8,7 +9,7 @@ class MapExploration(GroundedAction):
     def _evaluate_poses(self, candidates):
         pass
     
-class ElectErea(GroundedAction):
+class EreaElection(GroundedAction):
     def _gen_candidates(self):
         pass
 
@@ -33,3 +34,19 @@ class GAScheduler(GroundedAction):
     def _evaluate_poses(self, candidates):
         pass
 
+def main():
+    rosclient = roslibpy.Ros('10.244.1.176', port=9090)
+    rosclient.on_ready(lambda: print('is ROS connected: ', rosclient.is_connected))
+    rosclient.run()
+
+    mc = ros_mobile_client.MobileClient(rosclient, lambda: print('goal'))
+    ga_map = MapExploration('map_exploration', mc)
+    ga_ele = EreaElection('erea_election', mc)
+    ga_obs = Observation('observation', mc)
+    
+    pool_grounded = GroundedActionPool([ga_map, ga_ele, ga_obs])
+
+    sched = GAScheduler('top_scheduler', mc, pool_grounded)
+
+if __name__ == '__main__':
+    main()
